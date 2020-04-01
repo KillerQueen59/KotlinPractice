e30ef55e19c324aa2bb55d61f8ecf99f20abee80
ramadhantiba <fauzanramadhan59@gmail.com>
2020-04-01 23:30:44 +0700

    Practice 1

---
+++ KotlinPractice1/app/src/main/java/com/example/kotlinpractice1/Hai.kt
@@ -0,0 +1,20 @@
+package com.example.kotlinpractice1
+
+fun main() {
+    println("Hai Gais")
+}
+fun hai(){
+
+    val name = "ojan" // final
+    var age = 20 // bisa diubah
+    age += 2
+
+    // optimal value ?
+    var title: String? = null
+    println(title!!.length)
+
+    val school : String = ""
+    val company = ""
+
+    println("My Age is $age")
+}
